bb3d76d9372b43ea3c4906cfc5684e7beb5a2585
Darknight123prog <satishkumargupta6658@gmail.com>
2025-12-15 10:48:54 +0530

    added the order view by the user feature

--- Backend/Controllers/OrderController.js
+++ Backend/Controllers/OrderController.js
@@ -53,4 +53,39 @@ const getOrderDetails=async(req,res)=>{
     order_details:Order
   })
 }
-module.exports={createOrder,getOrderDetails};
+
+//adding the all orders view by the user;
+const OrderViewUser=async(req,res)=>{
+ const OrderId=req.params.id;
+ const Order=await OrderModel.findOne({_id:OrderId});
+
+
+ if(!Order){
+
+ return res.status(404).json({
+    success:false,
+    message:"Order not found"
+  })
+ }
+ else{
+  // console.log(Order);
+ const userId=req.RequestName._id;
+ 
+ if(userId.toString()!==Order.user.toString()){
+  return res.status(403).json({
+    success:false,
+    messsage:"Access is forbidden"
+  })
+ }
+ else{
+  return res.status(200).json({
+    success:true,
+    message:"Order details of the user is succesfully fetched",
+    Order_details:Order
+  })
+ }
+}
+}
+
+//
+module.exports={createOrder,getOrderDetails,OrderViewUser};
--- Backend/Routes/OrderRoutes.js
+++ Backend/Routes/OrderRoutes.js
@@ -1,9 +1,10 @@
 const express=require('express');
-const { createOrder,getOrderDetails } = require('../Controllers/OrderController');
+const { createOrder,getOrderDetails,OrderViewUser } = require('../Controllers/OrderController');
 const { auth, roleBasedAccess } = require('../Utils/Authetication');
 const OrderRouter=express.Router();
 
 
 OrderRouter.route('/createOrder').post(auth,createOrder);
+OrderRouter.route('/viewUserOrder/:id').post(auth,OrderViewUser);
 OrderRouter.route('/admin/getOrderDetails').post(auth,roleBasedAccess('admin'),getOrderDetails);
 module.exports=OrderRouter;
