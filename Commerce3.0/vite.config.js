44ce3b51aa0f52ddba4637b66337cf08d32c6a7c
Darknight123prog <satishkumargupta6658@gmail.com>
2026-01-15 16:04:48 +0530

    fix 01 for the payment gateway integration

--- Commerce3.0/vite.config.js
+++ Commerce3.0/vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
+   base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
